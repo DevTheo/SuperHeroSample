a7a0a0235011e3076db38f4f3231742a7e88c0a0
jkimb <jkimble@gmail.com>
2024-08-31 18:43:37 -0400

    Added Navigation (and fixed blazor to work with Htmx

---
+++ SuperheroSample/wwwroot/htmx-blazor.js
@@ -0,0 +1,7 @@
+﻿// An enhanced load allows users to navigate between different pages
+console.log("htmx loaded");
+Blazor.addEventListener("enhancedload", function () {
+    console.log("updating htmx tags");
+    // HTMX need to reprocess any htmx tags because of enhanced loading
+     setTimeout(()=> htmx.process(document.body), 100);
+});
